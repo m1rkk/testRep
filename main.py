0d227ee1abf1637c856ff28e6674035825fb4628
m1rkk <mrakkus15@gmail.com>
2023-12-16 13:45:52 +0200

    started doing decoder

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 text = str(input())
 text = text.upper()
-chars = []
-ans = ''
+text_to_array = []
+morza_ans = ''
 morza = {"A": ".-", "B": "-..", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.", "H": "...", "I": "..", "J": ".---", "K": "-.-", "L": ".-..", "M": "--","O": "---", "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-", "U": "..-", "V": "...-", "W":".--", "X": "-..-", "Y": "-.--", "Z": "--.."}
 for c in text:
-    chars.append(c)
-for letter in chars:
+    text_to_array.append(c)
+for letter in text_to_array:
     if morza.__contains__(letter):
-        ans += " " + morza[letter]
+        morza_ans += " " + morza[letter]
 
-print(ans)
+print(morza_ans)
+
+morza_to_array = morza_ans.split(" ")
+
+
+
