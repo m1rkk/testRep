f39552fc2d5fbf1808c2dc7f157a9307e1b3a3a6
m1rkk <mrakkus15@gmail.com>
2023-12-16 13:36:23 +0200

    final version

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 text = str(input())
+text = text.upper()
 chars = []
+ans = ''
+morza = {"A": ".-", "B": "-..", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.", "H": "...", "I": "..", "J": ".---", "K": "-.-", "L": ".-..", "M": "--","O": "---", "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-", "U": "..-", "V": "...-", "W":".--", "X": "-..-", "Y": "-.--", "Z": "--.."}
 for c in text:
     chars.append(c)
-print(chars)
+for letter in chars:
+    if morza.__contains__(letter):
+        ans += morza[letter]
+
+print(ans)
